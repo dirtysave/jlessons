68832ceac970e4836ea148c1280e8d002159f1e4
Alexey Slivashkin <alsav@yandex.ru>
2021-05-12 17:05:33 +0400

    second commit with MainClass

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,19 @@
-public class MainClass {
-    public static void main(String args[]) {
+import lesson1.*;
 
+public class MainClass {
+    public static void main(String[] args) {
+        Car jag = new Car();
+        Scate scate = new Scate();
+        Motorcycle bike = new Motorcycle();
+
+        Person human = new Person("Иван Иваныч");
+        human.Start(jag);
+        human.Stop(jag);
+
+        human.Start(scate);
+        human.Stop(scate);
+
+        human.Start(bike);
+        human.Stop(bike);
     }
 }
